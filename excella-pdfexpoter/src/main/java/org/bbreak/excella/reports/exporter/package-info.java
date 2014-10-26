a96a4af81b783e0134ed94a974525fd00ed32c50
kamisono-bb <kamisono@bbreak.co.jp>
2014-10-26 21:51:57 +0900

    初期インポート

---
+++ excella-pdfexpoter/src/main/java/org/bbreak/excella/reports/exporter/package-info.java
@@ -0,0 +1,33 @@
+/*************************************************************************
+ *
+ * Copyright 2014 by bBreak Systems.
+ *
+ * ExCella Core - ExcelファイルをJavaから利用するための共通基盤
+ *
+ * $Id$
+ * $Revision$
+ *
+ * This file is part of ExCella Core.
+ *
+ * ExCella Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * ExCella Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the COPYING.LESSER file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with ExCella Core.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+/**
+ * TODO: クラスの説明
+ *
+ * @since TODO: 対応バージョン
+ */
+package org.bbreak.excella.reports.exporter;
